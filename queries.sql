500a852f56b04d548ba225bb757e188b5e1c522e
althafaz <52558189+althafaz@users.noreply.github.com>
2024-06-11 23:16:17 -0400

    Task 1 & 2 is completed

--- queries.sql
+++ queries.sql
@@ -1,7 +1,10 @@
 --Part 1
-
+id - int
+employer - varchar
+name - varchar
+skills - varchar
 --Part 2
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Please enter location")
+    @Size(min = 3 ,max = 50, message = "Number letters cannot be exceed more than 50 and less than 3")
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer (){}
 }
