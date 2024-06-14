d64fe333c1698af6496651a409e2561d9ca0cf23
althafaz <52558189+althafaz@users.noreply.github.com>
2024-06-13 20:52:33 -0400

    Graded Assignment 4- completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -11,6 +16,14 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "Please enter location")
     @Size(min = 3 ,max = 50, message = "Number letters cannot be exceed more than 50 and less than 3")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -14,9 +18,17 @@ public class Skill extends AbstractEntity {
         return description;
     }
 
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
     public void setDescription(String description) {
         this.description = description;
     }
 
     public Skill(){}
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
 }
